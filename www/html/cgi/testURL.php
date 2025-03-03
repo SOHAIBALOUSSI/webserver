ed8f4236651e4889cc5c77ba2365469b4ecc7dcc
moel-gha <moel-gha@c3r3p3.1337.ma>
2025-03-03 13:26:20 +0000

    fix msg configfile and add uploads folder

--- www/html/cgi/testURL.php
+++ www/html/cgi/testURL.php
@@ -13,15 +13,6 @@ echo "<html>\n";
 echo "<head><title>PHP CGI Test</title></head>\n";
 echo "<body>\n";
 echo "<h1>Hello, $name! your age is $age</h1>\n";
-
-echo "<p>Environment variables:</p>\n";
-echo "<pre>\n";
-// Display some CGI environment variables
-echo "REQUEST_METHOD: " . $_SERVER['REQUEST_METHOD'] . "\n";
-echo "HTTP_HOST: " . $_SERVER['HTTP_HOST'] . "\n";
-echo "REMOTE_ADDR: " . $_SERVER['REMOTE_ADDR'] . "\n";
-echo "</pre>\n";
-
 echo "</body>\n";
 echo "</html>\n";
 
